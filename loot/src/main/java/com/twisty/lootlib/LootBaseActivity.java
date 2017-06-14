28600cffc895cf6e3312450b73a2a30dadfe50ae
twisty <twisty@vip.qq.com>
2017-06-14 13:55:27 +0800

    使用LocalBroadcastManager替换全局广播

--- loot/src/main/java/com/twisty/lootlib/LootBaseActivity.java
+++ loot/src/main/java/com/twisty/lootlib/LootBaseActivity.java
@@ -5,15 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class LootBaseActivity extends AppCompatActivity {
+    LocalBroadcastManager localBroadcastManager;
+
+
     BroadcastReceiver receiverExit;
     public String RECEIVER_EXIT_ACTION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
         RECEIVER_EXIT_ACTION = getPackageName() + "lootLib.exitAll";
         receiverExit = new BroadcastReceiver() {
             @Override
@@ -21,16 +26,16 @@ public class LootBaseActivity extends AppCompatActivity {
                 finish();
             }
         };
-        registerReceiver(receiverExit, new IntentFilter(RECEIVER_EXIT_ACTION));
+        localBroadcastManager.registerReceiver(receiverExit, new IntentFilter(RECEIVER_EXIT_ACTION));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(receiverExit);
+        localBroadcastManager.unregisterReceiver(receiverExit);
     }
 
     protected void exitApp() {
-        sendBroadcast(new Intent(RECEIVER_EXIT_ACTION));
+        localBroadcastManager.sendBroadcast(new Intent(RECEIVER_EXIT_ACTION));
     }
 }
